127bdf49c6b461362fc0002c160ebf0dc52f42b7
yjx199908 <1399590856@qq.com>
2020-06-23 00:39:19 +0800

    add some code of body-parser

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let express = require('express')
+var bodyp=require('body-parser');
+//post中间件接收数据
 
 let app = express()
+
+app.use(bodyp.urlencoded({ extended: false,limit:20*1024})); //extended 拓展模式  limit 最大接收数据
 
 app.use('/',(request,response,next)=>{
     console.log(request.url)
